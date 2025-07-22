7fde368206ec7b581b9328f47904ba35c89e7ee9
Sam V <sam.vanheer@outlook.com>
2025-07-22 20:55:57 +0200

    Add dependency on ImageMagick type to force inclusion, target x64 only

--- CSharpActionsTest/Program.cs
+++ CSharpActionsTest/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace CSharpActionsTest
+﻿using ImageMagick;
+
+namespace CSharpActionsTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            using MagickImage image = new();
         }
     }
 }
